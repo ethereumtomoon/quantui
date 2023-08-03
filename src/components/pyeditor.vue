ef630a55a8dfc76e987d9dc170713ace1665135a
xushengkai <xushengkai07@gmail.com>
2023-08-03 17:16:50 +0800

    1

---
+++ src/components/pyeditor.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+    <body>
+          <div v-bind:id="pad" 
+      ></div>
+    </body>  
+    
+      
+    </template>
+      <script setup>
+        let app = document.getElementById('main');
+        let child = app.firstChild;
+        while (child) {
+        app.removeChild(child);
+        child = app.firstChild
+        }
+        // Create a Pythonpad instance in the element with an id "pad". 
+        Pythonpad('main', {src: 'print("hello world")'})
+      </script>
+    
+    
